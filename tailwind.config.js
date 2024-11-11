183889a60f08dbb34abf83bef5ff716b3a5b0a47
amiko <basti.xachapuridze@gmail.com>
2024-11-12 00:47:52 +0400

    dasign system

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#24252C', // dark grey
+        secondary: '#1D1E27', // slightly darker grey
+        accent: '#6E6F83', // medium grey
+        textMain: '#E9E8EA', // light grey for text
+        success: '#4AFFAF', // bright green
+        warning: '#FF5A5A', // bright red
+        alert: '#FFCE00', // yellow
+        info: '#FFD085' // light yellow
+      },
+      fontFamily: {
+        mono: ['JetBrains Mono', 'monospace'],
+      },
+    },
   },
   plugins: [],
 }
